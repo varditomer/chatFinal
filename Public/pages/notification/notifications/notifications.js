d1c46aa51479cab383b110036d00809fb396b3b6
Dror Handler <hdror507@gmail.com>
2023-09-22 17:47:03 +0300

    notifications-table

--- Public/pages/notification/notifications/notifications.js
+++ Public/pages/notification/notifications/notifications.js
@@ -31,6 +31,7 @@ function read(notificationId) {
 
 
 const user = userService.getLoggedInUser()
+const titles = ['#', 'Notification', '']
 const yourUrl = "/api/notification/getNotifications/" + user.userCode;
 
 fetch(yourUrl)
@@ -42,26 +43,43 @@ fetch(yourUrl)
     console.log(notifications);
     if (notifications.length === 0) {
       strHtml +=
-        /*html*/
-        `<div><center>
+        `
                 <h5>
                 You have no new notifications</h5>
+ 
                 `;
+    }else{
+      strHtml+=`     <div class="row header">
+      <div key={title} class="cell">
+          ${titles[0]}
+      </div>
+      <div key={title} class="cell">
+          ${titles[1]}
+      </div>
+      <div  key={title} class="cell">
+          ${titles[2]}
+      </div>
+      </div>`
     }
 
-    notifications.forEach((notification) => {
+    notifications.forEach((notification, idx) => {
       let { id, content } = notification;
-      strHtml += /*html*/ `<div>
-                <div>
-                  <h6>#${content}
-                  <button type="submit"  style="color:white" onclick="read('${id}');"><img src="/assets/images/correct_icon.png" width="20"></button></h6>
-                  <br>
-                </div>
-              </div>`;
+      strHtml +=
+        `
+        <div class="row">
+          <div class="cell" data-title=${titles[0]}>
+            ${idx + 1}
+          </div>
+          <div class="cell" data-title=${titles[1]}>
+            ${(content)}
+          </div>
+          <div style="align-items: center;display:flex;justify-content:center" class="cell capitalize" data-title=${titles[2]}>
+            <button type="submit" style="color:white" onclick="read('${id}');"><img src="/assets/images/correct_icon.png" width="20"></button>
+          </div>
+          </div>
+          `
     });
-
-    console.log(strHtml);
-    document.getElementById("data").innerHTML = strHtml;
+    document.querySelector(".table-container").innerHTML = strHtml;
   });
 
 window.goToHomePage = userService.goToHomePage
