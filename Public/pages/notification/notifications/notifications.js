2a9b17dc22adbda66b2697a4cf0e70107616bd00
varditomer <tomer.vardi@gmail.com>
2023-09-22 18:32:10 +0300

    fix notification style

--- Public/pages/notification/notifications/notifications.js
+++ Public/pages/notification/notifications/notifications.js
@@ -44,19 +44,21 @@ fetch(yourUrl)
     if (notifications.length === 0) {
       strHtml +=
         `
-                <h5>
-                You have no new notifications</h5>
+                <h5 style="display: flex; gap: 10px; align-items: center; font-size: 20px; justify-content: center;">
+                  <i class="fas fa-bell-slash"></i>
+                  You have no new notifications
+                </h5>
  
                 `;
-    }else{
-      strHtml+=`     <div class="row header">
-      <div key={title} class="cell">
+    } else {
+      strHtml += `     <div class="row header">
+      <div key={title} class="cell" style="flex-basis: 10%;">
           ${titles[0]}
       </div>
       <div key={title} class="cell">
           ${titles[1]}
       </div>
-      <div  key={title} class="cell">
+      <div  key={title} class="cell" style="flex-basis: 10%;">
           ${titles[2]}
       </div>
       </div>`
@@ -66,18 +68,18 @@ fetch(yourUrl)
       let { id, content } = notification;
       strHtml +=
         `
-        <div class="row">
-          <div style="font-weight:600;" class="cell" data-title=${titles[0]}>
-            ${idx + 1}
+          <div class="row">
+            <div style="font-weight:600; flex-basis: 10%;" class="cell" data-title=${titles[0]} >
+              ${idx + 1}
+            </div>
+            <div class="cell" data-title=${titles[1]} style="font-size: 20px">
+              ${(content)}
+            </div>
+            <div style="align-items: center; display: flex; justify-content: center; flex-basis: 10%;" class="cell capitalize" data-title=${titles[2]}>
+              <button type="submit" style="border: none; background: transparent; color: #1E3050" onclick="read('${id}');"><i class="fas fa-check-square"></i></button>
+            </div>
           </div>
-          <div class="cell" data-title=${titles[1]}>
-            ${(content)}
-          </div>
-          <div style="align-items: center;display:flex;justify-content:center" class="cell capitalize" data-title=${titles[2]}>
-            <button type="submit" style="color:white" onclick="read('${id}');"><img src="/assets/images/correct_icon.png" width="20"></button>
-          </div>
-          </div>
-          `
+        `
     });
     document.querySelector(".table-container").innerHTML = strHtml;
   });
